c856ef0f5e74ae4f4ce733e5ce83f570174691e4
Pamela Xie <pjxie@ucsc.edu>
2024-02-22 23:37:18 -0800

    File writing

--- client/MainWindow.xaml.cs
+++ client/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Text;
+﻿using Microsoft.Win32;
+using System.IO;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -8,7 +10,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-
+//#include <iostream>
+//#include <fstream>
+//using namespace std;
 namespace client
 {
     /// <summary>
@@ -19,6 +23,42 @@ namespace client
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void ButtonGeneratePlugin_Click(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(deviceInfo.Text))
+            {
+                Stream myStream;
+                UnicodeEncoding uniEncoding = new UnicodeEncoding();
+                SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+                saveFileDialog1.Filter = "All files (*.*)|*.*";
+                saveFileDialog1.Title = "Save an Image File";
+                saveFileDialog1.ShowDialog();
+
+
+                if ((myStream = saveFileDialog1.OpenFile()) != null)
+                {
+                    // code to write the stream goes here.
+                    var sw = new StreamWriter(myStream, uniEncoding);
+                    string message = deviceInfo.Text;
+                    try
+                    {
+                        sw.Write(message);
+                        sw.Flush();
+                    }
+                    finally
+                    {
+                        sw.Dispose();
+                    }
+                    myStream.Seek(0, SeekOrigin.Begin);   
+                    myStream.Close();
+ 
+                }
+
+
+            }
+
         }
     }
 }
