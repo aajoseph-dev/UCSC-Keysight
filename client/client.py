015f3216093be9efd7e9703c09ca0efeedeea8c4
Huy Nguyen <duchuyn0411@gmail.com>
2024-03-09 15:17:33 -0800

    adding generate_zipfolder

--- client/client.py
+++ client/client.py
@@ -3,24 +3,30 @@ import requests
 #I used python for this as it would require the use of visual studio and a windows computer 
 #could be run from our macs for testing
 
- #api_url specific the request
-api_url = "http://127.0.0.1:5000/generate_xml" 
+#api_url specific the request
+api_url = "http://127.0.0.1:5000/generate_plugin" 
 
 #this question is passed to the chatbot, modify to change request
 question = "Give me a full list of front panel controls for E364xA power supply"  
 
 xml_string = {'name' : "download_file.py", 
-              'py_file' : '/Users/shaun/Desktop/115b/UCSC-Keysight/api/download_file.py'}
+              'py_file' : '/Users/shaun/Desktop/115b/UCSC-Keysight/api/download_file.py',
+              'file_path' : '/Users/shaun/Desktop/115b/UCSC-Keysight/test/E364xA'}
 
 
 test_string = {"name": "asdfasf.py", 
                "data": "print('hello world')\nprint('hello world2')"}
 
 # Since the api takes in a json request the question is formatted to accordingly 
-payload = {"question": question}
+payload = {"plugin_name" : "E364xA",
+            "question": question}
 
-# The response is sent to the specificed url, in our case its the generate plugin function
-response = requests.post(api_url, json=xml_string)
+# # The response is sent to the specificed url, in our case its the generate plugin function
+# file_name = requests.post(api_url, json=payload)
+# api_url = f"http://127.0.0.1:5000/download_file/{file_name}"
+# response = requests.post(api_url)
+
+response = requests.post(api_url, json=payload)
 
 #checks if a valid response was returned, printing the output if valid otherwise printing error code
 if response.status_code == 200:
@@ -28,9 +34,9 @@ if response.status_code == 200:
         result = response.json()
     else:
         try:
-            with open('download_file.xml', 'wb') as f:
+            with open('files.zip', 'wb') as f:
                 f.write(response.content)
-            print("downloaded succesfully")
+            print("Downloaded successfully")
         except Exception as e:
             print("error", e)
 else:
