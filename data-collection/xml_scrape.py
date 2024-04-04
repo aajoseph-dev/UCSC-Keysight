09513619b3d2fd54e6c0dbc6c0cae653bd601cc0
Madeline Miller <madelinemiller723@gmail.com>
2024-04-04 14:16:26 -0700

    scraping htm files

--- data-collection/xml_scrape.py
+++ data-collection/xml_scrape.py
@@ -1,15 +1,51 @@
+import webbrowser
 from bs4 import BeautifulSoup
+import urllib.parse
+import os
+import pdfkit
 
 
-file = open("/Users/shaun/Desktop/115b/UCSC-Keysight/documents/EDU36311A_K_01_04_03_01_00_01_04_01_01-v3/EDU36311A.sdl", "r")
-contents = file.read()
+topic_links = []
+path = "/Users/madeline/Desktop/UCSC-Keysight/documents/EDU/EDU36311A.sdl"
+path_to_topics = "/Users/madeline/Desktop/UCSC-Keysight/documents/EDU/docs/"
+
+with open(path, "r") as file:
+    contents = file.read()
 soup = BeautifulSoup(contents, 'xml')
 
 link = soup.find_all("HelpLink")
+# print("soup:", soup)
+# print(str(link))
+for data in link:
 
-with open('myfile23.txt', 'w+') as f:
-    for data in link:
-        f.write(data.get("url"))
-        f.write("\n")
+    data_str = str(data.get('url'))
+    data_str = data_str.replace('\\', '/')
+    complete_path = path_to_topics + data_str
+    sep = '#'
+    complete_path = complete_path.split(sep, 1)[0]
+    topic_links.append(complete_path)
 
-f.close()
+
+pdfkit.from_file(topic_links, 'out.pdf')
+
+# for link in topic_links:
+#     file_path = path + link
+#     file_path = file_path.replace('\\', '/')
+#    
+#     file_path = file_path.split(sep, 1)[0]
+#     print("file_path:", file_path)
+#     with open(file_path, "r") as file:
+#         contents = file.read()
+#         soup = BeautifulSoup(contents, "html.parser")
+#     links = soup.find_all('h3')
+#     with open('myfile1.txt', 'a') as f:
+#         for data in links:
+#             f.write(data.get_text())
+#             # print(data.get_text())
+#             f.write("\n")
+    # count += 1
+    # if count == 3:
+    #     break
+
+    # /Users/madeline/Desktop/UCSC-Keysight/documents/EDU/docs/Topics/2 - Programming Commands/60 - IEEE-488 Subsystem.htm
+    # /Users/madeline/Desktop/UCSC-Keysight/documents/EDU/docs/Topics/2 - Programming Commands/60 - IEEE-488 Subsystem.htm#ESE
