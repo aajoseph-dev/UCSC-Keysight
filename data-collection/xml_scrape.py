e6cfecbedeb4acd6df6595b99109ae0b01ae4be5
Madeline Miller <madelinemiller723@gmail.com>
2024-04-04 18:58:52 -0700

    cleaned code for xml scraping, updated requirements

--- data-collection/xml_scrape.py
+++ data-collection/xml_scrape.py
@@ -1,51 +1,26 @@
-import webbrowser
 from bs4 import BeautifulSoup
-import urllib.parse
-import os
 import pdfkit
 
+topic_links = [] # list to contain every programming command htm content
+path = "/Users/madeline/Desktop/UCSC-Keysight/documents/EDU/EDU36311A.sdl" # contains each url for the htm of each command
+path_to_topics = "/Users/madeline/Desktop/UCSC-Keysight/documents/EDU/docs/" # first part of htm
 
-topic_links = []
-path = "/Users/madeline/Desktop/UCSC-Keysight/documents/EDU/EDU36311A.sdl"
-path_to_topics = "/Users/madeline/Desktop/UCSC-Keysight/documents/EDU/docs/"
-
-with open(path, "r") as file:
+# open and read in sdl file
+with open(path, "r") as file: 
     contents = file.read()
 soup = BeautifulSoup(contents, 'xml')
 
+# collect only the urls under HelpLink in .sdl file for each programming command
 link = soup.find_all("HelpLink")
-# print("soup:", soup)
-# print(str(link))
-for data in link:
 
+# iterate through each programming command url in .sdl, and add the first part of htm, which is path_to_topics
+for data in link:
     data_str = str(data.get('url'))
-    data_str = data_str.replace('\\', '/')
+    data_str = data_str.replace('\\', '/') 
     complete_path = path_to_topics + data_str
     sep = '#'
-    complete_path = complete_path.split(sep, 1)[0]
+    complete_path = complete_path.split(sep, 1)[0] # remove characters starting from # and onwards
     topic_links.append(complete_path)
 
-
+# using the complete paths to the .htm for each programming command, put the content onto 1 .pdf file
 pdfkit.from_file(topic_links, 'out.pdf')
-
-# for link in topic_links:
-#     file_path = path + link
-#     file_path = file_path.replace('\\', '/')
-#    
-#     file_path = file_path.split(sep, 1)[0]
-#     print("file_path:", file_path)
-#     with open(file_path, "r") as file:
-#         contents = file.read()
-#         soup = BeautifulSoup(contents, "html.parser")
-#     links = soup.find_all('h3')
-#     with open('myfile1.txt', 'a') as f:
-#         for data in links:
-#             f.write(data.get_text())
-#             # print(data.get_text())
-#             f.write("\n")
-    # count += 1
-    # if count == 3:
-    #     break
-
-    # /Users/madeline/Desktop/UCSC-Keysight/documents/EDU/docs/Topics/2 - Programming Commands/60 - IEEE-488 Subsystem.htm
-    # /Users/madeline/Desktop/UCSC-Keysight/documents/EDU/docs/Topics/2 - Programming Commands/60 - IEEE-488 Subsystem.htm#ESE
