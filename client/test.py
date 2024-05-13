cb043d5d2673e2369784e3f2a807879c3ffa4081
pjxie <pjxie@ucsc.edu>
2024-05-12 17:18:40 -0700

    Implemented instrument plugin generation seperately
    Co-authored-by: Madeline Miller <MadelineMiller@users.noreply.github.com>
    Co-authored-by: Huy Nguyen <huy-nguy3n@users.noreply.github.com>

--- client/test.py
+++ client/test.py
@@ -29,14 +29,14 @@ def run_test(deviceName, category, commands, interface, progLang, role, useCase)
         print("Error:", response.status_code, response.text)
 
 
-# test = run_test("EDU36311A", "Power supply", [":DISPLay", ":MEASure"], "USB-B", "python", "As a test engineer, I want to create a plugin in python to test batteries", "generate_plugin")
+test = run_test("EDU36311A", "Power supply", [":DISPLay"], "USB-B", "python", "As a test engineer, I want to create a plugin in python to test batteries", "generate_plugin")
 # test = run_test("E8363B", "Network Analyzer", [":PRESet", ":FREQuency"], "", "python", "As a test engineer, I want to create a plugin in python to analyze networks", "generate_plugin")
 
 # PSG Signal Generator
 # test = run_test("E8663D", "PSG Signal Generator", [":CONTrast", ":DATA"], "", "python", "As a test engineer, I want to create a plugin in python to generate signals", "generate_plugin")
 
 # Digital Multimeters (DMM), 34470A
-test = run_test("34470A", "Digital Multimeter", [":CALCulate:SCALe:DB:REFerence", ":CALibration:STRing"], "", "python", "As a test engineer, I want to create a plugin in python to interface with a Digital Multimeter", "generate_plugin")
-
+# test = run_test("34470A", "Digital Multimeter", [":CALCulate:SCALe:DB:REFerence", ":CALibration:STRing"], "", "python", "As a test engineer, I want to create a plugin in python to interface with a Digital Multimeter", "generate_plugin")
+# test = run_test("34470A", "Digital Multimeter", [":CALCulate:SCALe:DB:REFerence"], "", "python", "As a test engineer, I want to create a plugin in python to interface with a Digital Multimeter", "generate_plugin")
 
 print(test)
